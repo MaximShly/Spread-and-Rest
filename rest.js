30a17acfa048f8b451a04473e123ba508d318da7
MaximShly <118232796+MaximShly@users.noreply.github.com>
2023-07-26 10:51:25 -0400

    Add files via upload

---
+++ rest.js
@@ -0,0 +1,8 @@
+// TODO: create the function sumAll
+
+console.log(sumAll(1, 2)); // expected output: 3
+console.log(sumAll(1, 2, 3)); // expected output: 6
+console.log(sumAll(1, 2, 3, 4)); // expected output: 10
+
+//don't change this line
+  module.exports = { sumAll };
